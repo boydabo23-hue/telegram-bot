19f73cfbf1adb971b97b3346d2e1b81926dbbb58
boydabo23-hue <boydabo23@gmail.com>
2026-05-07 09:51:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 CHANNEL = "@ThisIsVero"
-GROUP = "@bpoindo"
+GROUP = "@veyyooo"
 
 USERNAME_BOT = "bitchhubofficialBot"
 
